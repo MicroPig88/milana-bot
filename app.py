48f73e4648f5ce393134a305ca4e5e52ed5b3994
MicroPig88 <Erihon88@Gmail.com>
2025-11-27 04:18:31 +0700

    Bot v2.9

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from bot.main import forward, BOT_TOKEN
 
 flask_app = Flask(__name__)
 
+application = Application.builder().token(BOT_TOKEN).build()
+application.add_handler(MessageHandler(filters.ALL, forward))
+
 @flask_app.route("/")
 def home():
     return "Бот работает!", 200
@@ -14,8 +17,6 @@ def home():
 @flask_app.route(f"/webhook/{BOT_TOKEN}", methods=["POST"])
 def webhook():
     # перехватываем ВСЁ, что может содержать сообщение
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(MessageHandler(filters.ALL, forward))
     update = Update.de_json(request.get_json(force=True), Bot(token=BOT_TOKEN))
     # обрабатываем апдейт через приложение
     asyncio.create_task(application.process_update(update))
